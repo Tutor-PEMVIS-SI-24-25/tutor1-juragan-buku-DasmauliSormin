0703c5f6c133c7b72f23e209cce929e2e85cbcbe
DasmauliSormin <dasmauli06@gmail.com>
2024-09-04 21:27:20 +0700

    telah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -1,3 +1,17 @@
-// NIM - NAMA
-// NIM - NAMA
+// 12S24007 - Dasmauli Sormin
+// 12S24007 - Dasmauli Sormin
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String namalengkap;
+
+        namalengkap = input.nextLine();
+        System.out.println(namalengkap);
+    }
+}
+
 
